cf760a49336efbab4c45de31d7d397224dff2cd1
hirorogo <hiro114514@proton.me>
2025-08-09 22:49:51 +0900

    QUIZを作成

--- eslint.config.js
+++ eslint.config.js
@@ -39,3 +39,4 @@ export default [
     },
   },
 ]
+export default QuizData
--- src/data/quiz.js
+++ src/data/quiz.js
@@ -6,27 +6,37 @@ const quizData = [
     },
     {
         question: " 愛知総合工科高等学校の校訓は？",
-        options: ["夢志（ゆめ）実現、　輝かせ　知・技・心", "挑戦・創造・協力", "知識・技術・誠実"]
+        options: ["夢志（ゆめ）実現、　輝かせ　知・技・心", "挑戦・創造・協力", "知識・技術・誠実"],
         answerIndex: 0
     },
     {
         question: " 愛知総合工科の学科の数は",
-        options: ["8", "7", "10"]
+        options: ["8", "7", "10"],
         answerIndex: 1
     },
     {
         question: "愛知総合工科の１学年のクラスの数は",
-        options: ["6", "7", "8"]
+        options: ["6", "7", "8"],
         answerIndex: 2
     },
     {
-        question: " text",
-        options: ["選択肢１", "選択肢２", "選択肢３"]
+        question: "愛知総合工科高等学校にできる附属中学校の制服のネクタイの色は？",
+        options: ["紺", "黄", "赤"],
+        answerIndex: 1
+    },
+    {
+        question: "愛知総合工科高等学校の所在地は？",
+        options: ["名古屋市千種区", "名古屋市東区", "名古屋市西区"],
         answerIndex: 0
     },
     {
-        question: " text",
-        options: ["選択肢１", "選択肢２", "選択肢３"]
+        question: "愛知総合工科高等学校の工科にでてくる略称は？",
+        options: ["愛工", "愛総工", "愛知工科"],
+        answerIndex: 0
+    },
+    {
+        question: "愛知総合工科高等学校の校章に使われているモチーフは？",
+        options: ["歯車", "桜", "ペン"],
         answerIndex: 0
     },
 ];
