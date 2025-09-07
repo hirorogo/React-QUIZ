315d5e119f0c02b95b0dab7188660d8da1e11f95
hirorogo <hiro114514@proton.me>
2025-09-07 17:03:28 +0900

    feat: update quiz page to use dynamic button rendering and restore button styles

--- src/pages/Quizpage.jsx
+++ src/pages/Quizpage.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import Display from "../components/Display/Display";
 import quizData from "../data/quiz";
 import Button from "../components/Button/Button";
+import { useNavigation } from "react-router-dom";
 
 export default function Quizpage() {
-  const quizIndex = 0;
+  const [quizIndex, setQuizIndex] = useState(0);
+  const [answerLogs, setAnswerLogs] = useState([]);
+  const nabigation = useNavigation();
+  const MAX_QUIZ_LEN = quizData.length;
   const quiz = quizData[quizIndex];
+  const handleClick = (clickedIndex) => {
+    if (clickedIndex === quiz.answerIndex) {
+      setAnswerLogs((prev) => [...prev, true]);
+    } else {
+      setAnswerLogs((prev) => [...prev, false]);
+    }
+    setQuizIndex((prev) => prev + 1);
+  };
 
   return (
     <>
-      <Display>{`Q1. ${quiz.question}`}</Display>
-      <Button>{quiz.options[0]}</Button>
-      <Button>{quiz.options[1]}</Button>
-      <Button>{quiz.options[2]}</Button>
+      <Display>
+        {`Q${quizIndex + 1}. ${quiz.question}`}
+      </Display>
+      {quiz.options.map((option, index) => (
+        <Button key={`option-${index}`} onClick={() => handleClick(index)}>
+          {option}
+        </Button>
+      ))}
     </>
   );
 }
